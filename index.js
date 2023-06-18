9d9e1423fd6ba9dbb7269f1040aed89c4a96daca
Filip Wojtowicz <wojtowicz2222@gmail.com>
2023-06-19 00:05:52 +0200

    next steps in js

--- index.js
+++ index.js
@@ -14,8 +14,12 @@ addNewPositionIncome.addEventListener("submit", (event) => {
   event.preventDefault();
   const name = event.target.incomeName.value;
   const amount = event.target.incomeAmount.value;
-  incomes.push({ name, amount });
+  const id = Math.random();
+
+  incomes.push({ name, amount, id });
+  addIncome(name, amount, id);
   updateTotalIncomes();
+  finalScoreCalculation();
 });
 
 function updateTotalIncomes() {
@@ -31,8 +35,12 @@ addNewPositionExpenses.addEventListener("submit", (event) => {
   event.preventDefault();
   const name = event.target.expensName.value;
   const amount = event.target.expensAmount.value;
-  expenses.push({ name, amount });
+  const id = Math.random();
+
+  expenses.push({ name, amount, id });
+  addExpenses(name, amount, id);
   updateTotalExpenses();
+  finalScoreCalculation();
 });
 
 function updateTotalExpenses() {
@@ -40,4 +48,37 @@ function updateTotalExpenses() {
     .map((expens) => Number(expens.amount))
     .reduce((a, b) => a + b, 0);
   totalExpenses.textContent = `${total} PLN`;
+}
+
+function finalScoreCalculation() {
+  const allIncomes = incomes
+    .map((income) => Number(income.amount))
+    .reduce((a, b) => a + b, 0);
+  const allExpenses = expenses
+    .map((expens) => Number(expens.amount))
+    .reduce((a, b) => a + b, 0);
+
+  const finalresult = allIncomes - allExpenses;
+
+  finalScore.textContent = `In this month you can spend ${finalresult} PLN`;
+}
+
+function addIncome(name, amount, id) {
+  const item = document.createElement("li");
+  item.textContent = `${name}: ${amount} PLN`;
+
+  const editionBtn = document.createElement("button");
+
+  incomesList.appendChild(editionBtn);
+  incomesList.appendChild(item);
+}
+
+function addExpenses(name, amount, id) {
+  const item = document.createElement("li");
+  item.textContent = `${name}: ${amount} PLN`;
+
+  const editionBtn = document.createElement("button");
+
+  expensesList.appendChild(editionBtn);
+  expensesList.appendChild(item);
 }
